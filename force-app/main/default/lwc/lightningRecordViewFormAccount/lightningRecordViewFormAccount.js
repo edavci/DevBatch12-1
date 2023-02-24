dc69d463dd2e7c4f2aa3735d03320e52fe066da8
eda avci <edavci1730@gmail.com>
2023-02-24 17:24:36 -0500

    LWC2

---
+++ force-app/main/default/lwc/lightningRecordViewFormAccount/lightningRecordViewFormAccount.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+
+export default class LightningRecordViewFormAccount extends LightningElement {
+    recordId = "001Do000003DzgfIAC";
+    objectName = ACCOUNT_OBJECT;
+}
