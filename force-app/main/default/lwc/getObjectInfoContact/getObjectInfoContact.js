69ba10c08e853a18ae6b0a5b004d9d737f7539fb
eda avci <edavci1730@gmail.com>
2023-03-03 08:45:40 -0500

    LWC LAST

---
+++ force-app/main/default/lwc/getObjectInfoContact/getObjectInfoContact.js
@@ -0,0 +1,19 @@
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { LightningElement, wire } from 'lwc';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+export default class GetObjectInfoContact extends LightningElement {
+    contactDefRtId;
+    contactEmpRtId;
+    @wire(getObjectInfo, {objectApiName: CONTACT_OBJECT})
+    objectInfoHandler({data, error}) {
+        if(data) {
+            console.log(data);
+            this.contactDefRtId = data.defaultRecordTypeId;
+            const rtids = data.recordTypeInfos;
+            this.contactEmpRtId = Object.keys(rtids).find(rtid => rtids[rtid].name === "Employee Contact");
+        }
+        if(error) {
+            console.log(error);
+        }
+    }
+}
